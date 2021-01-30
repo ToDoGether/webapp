c3a94e7b0c6ac84e3844c02b0c90e718f9d3e2bc
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-01-30 11:49:23 +0100

    generate scaffold team

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_103704) do
+ActiveRecord::Schema.define(version: 2021_01_30_104717) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "duedate"
     t.integer "worktype"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
