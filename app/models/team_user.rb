73901242c9dff7686f958d349e3dd0876347f76d
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-01-30 16:46:05 +0100

    fix changes from code review

--- app/models/team_user.rb
+++ app/models/team_user.rb
@@ -1,12 +1,4 @@
 class TeamUser < ApplicationRecord
   belongs_to :user
   belongs_to :team
-
-#   attr_accessor :team_id, :user_id, :is_admin
-
-#   def initialize(team_id, user_id, is_admin)
-#     @team_id = team_id
-#     @user_id = user_id
-#     @is_admin = is_admin
-#   end
 end
