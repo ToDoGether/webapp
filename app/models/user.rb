44b2de49a4af02cf48a579e190a201c92e9b50c1
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-01-30 14:12:29 +0100

    add join_model team_user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :team_users
+  has_many :teams, :through => :team_users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_30_104717) do
+ActiveRecord::Schema.define(version: 2021_01_30_130212) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
     t.datetime "duedate"
     t.integer "worktype"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "team_users", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "team_id"
+    t.boolean "is_admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
