075fcb607af598e3a449edbe6808cf44b6305937
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-02-03 18:08:47 +0100

    check permissions on creating new task

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,8 @@ class User < ApplicationRecord
   has_many :tasks, through: :user_tasks
 
   has_many :subjects, -> { order(:name).distinct }, through: :teams
+
+  def is_any_admin?
+    team_users.where(is_admin: true).any?
+  end
 end
