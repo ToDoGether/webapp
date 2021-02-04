0420a20190f18d4f85c150753e476c74622fa10b
Josef Markus Mitterbauer <jmitterbauer.mmt-b2019@fh-salzburg.ac.at>
2021-02-04 02:31:35 +0100

    Changes datatype of worktype to boolean

---
+++ db/migrate/20210204011017_change_data_type_for_work_type.rb
@@ -0,0 +1,5 @@
+class ChangeDataTypeForWorkType < ActiveRecord::Migration[6.1]
+  def change
+    change_column :tasks, :worktype, 'boolean USING (CASE WHEN worktype > 0 THEN TRUE ELSE FALSE END)'
+  end
+end
