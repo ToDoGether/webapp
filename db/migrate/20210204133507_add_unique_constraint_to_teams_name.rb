974cfbf9e381817cf42c7fbf8ca55615f157cd44
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-02-04 15:13:55 +0100

    migrate db: make taks names unique

---
+++ db/migrate/20210204133507_add_unique_constraint_to_teams_name.rb
@@ -0,0 +1,5 @@
+class AddUniqueConstraintToTeamsName < ActiveRecord::Migration[6.1]
+  def change
+    add_index(:teams, :name, unique: true)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_110653) do
+ActiveRecord::Schema.define(version: 2021_02_04_133507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 2021_02_04_110653) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["name"], name: "index_teams_on_name", unique: true
   end
 
   create_table "user_tasks", force: :cascade do |t|
