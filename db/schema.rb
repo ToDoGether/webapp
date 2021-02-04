1f2249cd3ed8b515ebf8ff49de5f43eaeba4ac45
Josef Markus Mitterbauer <jmitterbauer.mmt-b2019@fh-salzburg.ac.at>
2021-02-04 18:07:42 +0100

    New schema file after migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_04_165621) do
+ActiveRecord::Schema.define(version: 2021_02_04_170510) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,6 @@ ActiveRecord::Schema.define(version: 2021_02_04_165621) do
     t.string "name"
     t.date "duedate"
     t.boolean "worktype"
-    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "subject_id", null: false
