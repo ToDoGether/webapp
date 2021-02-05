4ce75891df883d70a37d3dddc213c9ed94840cb9
Bernhard Schmidhuber <bernhard@bschmidhuber.at>
2021-02-05 11:38:35 +0100

    bugfix validation uniqueness team name

--- app/models/team.rb
+++ app/models/team.rb
@@ -5,5 +5,5 @@ class Team < ApplicationRecord
   accepts_nested_attributes_for :subjects, :allow_destroy => true
   accepts_nested_attributes_for :team_users
 
-  validates :name, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: 'only allows letters and numbers' }
+  validates :name, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z0-9_-]+\z/, message: 'only allows letters and numbers' }
 end
